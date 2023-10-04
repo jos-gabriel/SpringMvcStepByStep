c8ff0654f2ec58a37ddb76363dc2a0edd4907d9a
jos-gabriel <jos.gabrie@gmail.co m>
2023-10-04 00:05:40 -0600

    refactor(S2)13 MVC RequestMapping

--- Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     //@RequestMapping (value = "/index", method = RequestMethod.GET)
-    @GetMapping (value = {"/index", "/", "/home"})
+    @GetMapping (value = {"/index", "/", "","/home"})
     public String index(Model model){
         model.addAttribute("titulo", "Hola Spring Framework Model");
         return "index";
