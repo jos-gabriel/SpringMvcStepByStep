bbb1765f8667570e7d6babd29b42c41deceaa19f
jos-gabriel <jos.gabrie@gmail.co m>
2023-10-03 23:39:34 -0600

    refactor(S2)13 MVC Pasar datos con Model o ModelMap

--- Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +10,8 @@ public class IndexController {
 
     //@RequestMapping (value = "/index", method = RequestMethod.GET)
     @GetMapping (value = {"/index", "/", "/home"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("titulo", "Hola Spring Framework Model");
         return "index";
     }
 }
