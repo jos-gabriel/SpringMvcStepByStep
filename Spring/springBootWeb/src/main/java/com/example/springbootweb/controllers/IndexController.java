1227b4e5b4e28bc5807f6b95122a7d6db45c0a17
jos-gabriel <jos.gabrie@gmail.co m>
2023-10-04 00:31:08 -0600

    refactor(S2)13 MVC Objeto Model

--- Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ Spring/springBootWeb/src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springbootweb.controllers;
 
+import com.example.springbootweb.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -15,5 +16,18 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("titulo", "Hola Spring Framework Model");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(ModelMap model){
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellido("Pérez");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
     }
 }
